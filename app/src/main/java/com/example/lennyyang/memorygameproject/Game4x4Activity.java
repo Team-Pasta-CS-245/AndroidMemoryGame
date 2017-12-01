bb921dec7579b0b2810c213331e6de3bcb752709
Lenny Yang <lennyiswise@gmail.com>
2017-11-30 20:23:36 -0800

    up

--- app/src/main/java/com/example/lennyyang/memorygameproject/Game4x4Activity.java
+++ app/src/main/java/com/example/lennyyang/memorygameproject/Game4x4Activity.java
@@ -145,7 +145,7 @@ public class Game4x4Activity extends AppCompatActivity implements View.OnClickLi
 
                     isBusy = false;
                 }
-            }, 250);
+            }, 500);
         }
     }
 }
