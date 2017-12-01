b87e4ce07b23660e731c9753cc544d11fc39ca93
Lenny Yang <lennyiswise@gmail.com>
2017-11-30 17:17:23 -0800

    First

---
+++ app/src/main/java/com/example/lennyyang/memorygameproject/MemoryButton.java
@@ -0,0 +1,81 @@
+package com.example.lennyyang.memorygameproject;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatDrawableManager;
+import android.widget.GridLayout;
+
+import com.example.lennyyang.memorygameproject.R;
+import android.widget.Button;
+
+/**
+ * Created by lennyyang on 11/30/17.
+ */
+
+//Button
+public class MemoryButton extends AppCompatButton {
+
+    protected int row;
+    protected int column;
+    protected int frontDrawableId;
+
+    protected boolean isFlipped = false;
+    protected boolean isMatched = false;
+
+    protected Drawable front;
+    protected Drawable back;
+
+    @SuppressLint("RestrictedApi")
+    public MemoryButton(Context context, int r, int c, int frontImageDrawableId ){
+        super(context);
+
+        row = r;
+        column = c;
+        frontDrawableId = frontImageDrawableId;
+
+        front = AppCompatDrawableManager.get().getDrawable(context, frontImageDrawableId);
+        back = AppCompatDrawableManager.get().getDrawable(context, R.drawable.button_question);
+
+        setBackgroundDrawable(back);
+
+        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r), GridLayout.spec(c));
+
+        tempParams.width = (int) getResources().getDisplayMetrics().density * 50;
+        tempParams.height = (int) getResources().getDisplayMetrics().density * 50;
+
+        setLayoutParams(tempParams);
+
+//        Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+
+    public int getFrontDrawableId() {
+        return frontDrawableId;
+    }
+
+    public void flip(){
+        if(isMatched){
+            return;
+
+        }
+
+        if(isFlipped){
+            setBackgroundDrawable(back);
+            isFlipped = false;
+        }
+        else{
+            setBackgroundDrawable(front);
+            isFlipped = true;
+        }
+    }
+}
